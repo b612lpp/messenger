abe91bbdace10583100a45e7d323a80d6c27ea77
Alex Smith <asoinf@gmail.com>
2021-03-04 19:33:32 +0300

    03

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MainPage from './MainPage.jsx';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainPage/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Components/Message.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Message extends Component {
+
+    render() {
+        return (
+            
+            <div style={{ backgroundColor: this.props.author === "bot" ? 'red' : 'green' }}>
+                {this.props.author} says {this.props.text}
+            </div>
+        )
+    }
+}
+export default Message
